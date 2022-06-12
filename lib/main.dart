360f636b77b264169eb8bbfd7b8162d8646c4391
blast06 <lacalculadora06@hotmail.com>
2022-06-12 16:14:49 -0400

    added main method to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:score_domino/src/provider/app_preferences_provider.dart';
 import 'package:score_domino/src/provider/score_provider.dart';
+import 'package:package_info/package_info.dart';
 import 'package:score_domino/src/routes/routes.dart';
 
 
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final appVersion = 'v${packageInfo.version}';
+  final preferences = AppPreferencesProvider(appVersion);
+  await preferences.initPreferences();
+
+  // final configuratedApp = AppConfig(
+  //   env: AppEnv.dev,
+  //   child: MyApp(preferences),
+  //   nameAppFirebase: 'score-domino-prod',
+  // );
+  runApp(MyApp(preferences));
+}
 
 //TODO: ADD "SHARE TO SOCIAL MEDIA" 
 class MyApp extends StatelessWidget {

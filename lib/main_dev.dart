2a8c6a80b493e41365000ef811e59d42c5efbd2a
Blast06 <lacalculadora06@hotmail.com>
2022-08-09 00:00:03 -0400

    fixed bug at splash and removed firebase

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 import 'package:package_info/package_info.dart';
 import 'package:score_domino/main.dart';
 import 'package:score_domino/src/provider/app_preferences_provider.dart';
@@ -8,14 +9,15 @@ import 'package:score_domino/src/resources/app_config.dart';
 import 'src/controllers/AdmobController.dart';
 
 void main() async {
-  
+  Logger log = Logger();
+
   WidgetsFlutterBinding.ensureInitialized();
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
   final appVersion = 'v${packageInfo.version}';
   final preferences = AppPreferencesProvider(appVersion);
   await preferences.initPreferences();
   Get.lazyPut(() => AdmobController());
-
+  log.v("Testing on ios");
   // final configuratedApp = AppConfig(
   //   env: AppEnv.dev,
   //   child: MyApp(preferences),
